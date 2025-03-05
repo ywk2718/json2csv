5537d2c4878da66c78bb1926a9637cc155c4735f
ywk2718 <ywaka2718@gmail.com>
2025-03-05 10:42:19 +0900

    [Add] newly created

---
+++ json2csv.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+#Usage python3 json2csv.py -i input.json -o output.csv/output.tsv -f output_format
+import sys
+import os
+import argparse
+import pandas as pd
+import json
+
+parser = argparse.ArgumentParser(description='python script for converting json format to csv/tsv format')
+
+parser.add_argument('-i', '--input', required=True, help='input json file')
+parser.add_argument('-o', '--output', required=True, help='output file')
+parser.add_argument('-t', '--out_tsv', action='store_true', help='output as tsv format')
+parser.add_argument('--no_header', action='store_true', help="output without header" )
+
+args = parser.parse_args()
+
+fileIn = args.input
+fileOut = args.output
+outTsv = args.out_tsv
+noHeader = args.no_header
+
+#load input.json file
+json_open = open(fileIn, 'r')
+json_load = json.load(json_open)
+
+#expand nested json format with json_normalize
+df_json = pd.json_normalize(json_load)
+
+#command option
+cmd = "df_json.to_csv(fileOut, index=False, encoding='utf-8'"
+if outTsv == True:
+    cmd = cmd+", sep='\t'"
+if noHeader == True:
+    cmd = cmd+", header=False"
+cmd = cmd+")"
+eval(cmd)
